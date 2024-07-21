0ae1da5661a1abdffffad6c6c6cab64e63ef46ab
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-07-21 17:21:38 +0300

    next url

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        images: {
-          remotePatterns: [
-            {
-              hostname: 'res.cloudinary.com',
-            },
-          ],
-        },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  env: {
+    MONGODB_URL: process.env.MONGODB_URL,
+    ECOMMERCE_STORE_URL: process.env.ECOMMERCE_STORE_URL,
+    ADMIN_DASHBOARD_URL: process.env.ADMIN_DASHBOARD_URL,
+  },
 };
 
 export default nextConfig;
-
